55783a465d776df4ed4853d97e0d48e6bd01ca5b
Vigneshwaran <manivigneshwaran88@gmail.com>
2018-01-29 07:04:42 +0530

    My Project setup Completed

---
+++ shoppingbackend/src/main/java/my/vig/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package my.vig.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
